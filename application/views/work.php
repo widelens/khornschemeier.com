011c81e13dc4d9fccb46dd0465001a039287e904
Kevin Hornschemeier <khornschemeier@gmail.com>
2013-03-12 16:34:55 -0400

    Added new work experiences and new copy

--- application/views/work.php
+++ application/views/work.php
@@ -36,11 +36,37 @@
 </div>
 
 <div class="experience">
+	<h2>LamsonEnia Design</h2>
+	<div class="caption">
+		<p>
+			A design and marketing agency where I am the Director of Technology and managing all aspects of our technical stack. I work on maintaining existing clients sites, building out new sites, and managing a few remote Flash developers for a large client's site. I am also building the technical architecture for an in-house CMS to be used on future client sites.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge short">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>Git</li>
+		</ul>
+		<ul class="knowledge long">
+			<li>jQuery</li>
+			<li>Javascript</li>
+			<li>Google Analytics</li>
+			<li>Codeigniter</li>
+			<li>Photoshop</li>
+		</ul>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
 	<h2>Nomad</h2>
 	<img src="/img/work/nomad.jpg" alt="nomad" class="hidden-phone">
 	<div class="caption">
 		<p>
-			Personal side project for tracking hikes and photos<br />
+			This was a personal side project where I could capture and upload hikes that I took (via KML files) and photos that I captured along the way and the site would showcase my hikes with Google Maps and the photos shown at the location they were taken. I created an admin area where I could upload the hikes and photos by location, and also edit and delete them. I also wrote my own form validation for javascript and PHP as an exercise in creating a good UI on the front-end and an easy way to reuse validation rules on both the front-end and back-end.<br />
 			<br />
 			Technologies used:<br />
 		</p>
@@ -52,13 +78,15 @@
 			<li>Git</li>
 		</ul>
 		<ul class="knowledge medium">
+			<li>JSON</li>
+			<li>AJAX</li>
 			<li>jQuery</li>
 			<li>Javascript</li>
 			<li>CodeIgniter</li>
-			<li>Photoshop</li>
-			<li>Google Maps</li>
 		</ul>
 		<ul class="knowledge long">
+			<li>Photoshop</li>
+			<li>Google Maps</li>
 			<li>Google My Tracks</li>
 			<li>KML File Format</li>
 			<li>Google Analytics</li>
@@ -76,7 +104,7 @@
 	<img src="/img/work/venturepax.jpg" alt="venturepax.com" class="hidden-phone">
 	<div class="caption">
 		<p>
-			Cincinnati start-up<br />
+			Venturepax was a Cincinnati start-up where I was brought on as the technical co-founder. The site allowed users to search for outdoor activities based on the user's location and the specific activity they were looking to do. Content was curated from multiple sources including an outdoor book publisher, publicly available content, and user submitted content. Initially I helped maintain and expand the version of the site that existed when I was brought on board. I also initiated and managed an outside source for the first mobile app. After the decision was made to pivot and redesign the experience, I took over the architecture and planning for the technology pieces. The site was rebuilt from the ground up with a new database schema, new framework, and new functionality. An iPhone developer was hired who I coordindated with on the new mobile app. I eventually had to leave because funding had run out.<br />
 			<br />
 			Technologies used:<br />
 		</p>
@@ -99,6 +127,211 @@
 			<li>Facebook API</li>
 			<li>Google Maps</li>
 			<li>CSS Sprites</li>
+		</ul>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
+	<h2>The Scholar App</h2>
+	<img src="/img/work/thescholarapp.jpg" alt="The Scholar App" class="hidden-phone">
+	<div class="caption">
+		<p>
+			An application I co-founded and built on the Facebook platform. It provided students with a centralized application that let them meet and collaborate with classmates, participate in class discussions, exchange notes, and receive notifications for exams. It could be used through Facebook, iPhone, or Android. Colleges were curated from online sources and the classes were curated from teachers and students submissions.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge short">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>svn</li>
+		</ul>
+		<ul class="knowledge">
+			<li>jQuery</li>
+			<li>Javascript</li>
+			<li>Facebook Platform</li>
+			<li>Google Analytics</li>
+			<li>Epiphany MVC</li>
+		</ul>
+		<div class="clearfix"></div>
+		<div class="actions">
+			<a href="http://www.crunchbase.com/company/the-scholar-app" target="_blank" class="btn btn-small btn-primary">View on CrunchBase</a>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
+	<h2>Textbook Revolt</h2>
+	<img src="/img/work/textbookrevolt.jpg" alt="Textbook Revolt" class="hidden-phone">
+	<div class="caption">
+		<p>
+			Another site I co-founded and built, Textbook Revolt began addressing the problem of high priced textbooks by experimenting with a completely free college textbook exchange named Socialbib. Students could list the textbooks they were done with and view the available textbooks from other students and create exchanges. The site received many accolades from students who were previously dissatisfied with the options available to them. As a result, students from over 1,300 schools nationwide joined Socialbib and posted over $1,000,000 in college textbooks. The site was sold.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge short">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>svn</li>
+		</ul>
+		<ul class="knowledge medium">
+			<li>JSON</li>
+			<li>AJAX</li>
+			<li>jQuery</li>
+			<li>Javascript</li>
+			<li>Google Maps</li>
+		</ul>
+		<ul class="knowledge long">
+			<li>Facebook API</li>
+			<li>Stamps.com API</li>
+			<li>Amazon.com API</li>
+			<li>Google Analytics</li>
+			<li>Epiphany MVC</li>
+		</ul>
+		<div class="clearfix"></div>
+		<div class="actions">
+			<a href="http://www.crunchbase.com/company/textbook-revolt" target="_blank" class="btn btn-small btn-primary">View on CrunchBase</a>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
+	<h2>Publicitweet</h2>
+	<img src="/img/work/publicitweet.jpg" alt="Publicitweet" class="hidden-phone">
+	<div class="caption">
+		<p>
+			PubliciTweet was a social media marketing application that I co-founded and built from the ground up. It aimed at providing in-depth analytics for Twitter campaigns. The analytics went beyond the number of clicks and provided detailed information on which users had the most referrals and the overall reach. It allowed businesses, musicians or other groups to communicate with their followers on Twitter and track the success of those campaigns. The site provided tools to send mass direct messages, determine which followers referred the most clicks, and where the clicks came from. The site was sold.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge short">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>svn</li>
+		</ul>
+		<ul class="knowledge">
+			<li>jQuery</li>
+			<li>Javascript</li>
+			<li>Twitter API</li>
+			<li>Google Analytics</li>
+			<li>Google Maps</li>
+			<li>Epiphany MVC</li>
+		</ul>
+		<div class="clearfix"></div>
+		<div class="actions">
+			<a href="http://www.crunchbase.com/company/publicitweet" target="_blank" class="btn btn-small btn-primary">View on CrunchBase</a>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
+	<h2>Photagious</h2>
+	<img src="/img/work/photagious.jpg" alt="Photagious" class="hidden-phone">
+	<div class="caption">
+		<p>
+			Photagious was a photo sharing and managing application with the unique ability to create slideshows of photos and add graphical enhancements on top of them. When hired, I helped the technical co-founder with building a new site from the ground up. I was also integral in building out the forums and helped with most of the functionality of the site. The site had approximately 1 million uploaded photos and 10,000 users before having to be shut down due to lack of funding.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge short">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>svn</li>
+		</ul>
+		<ul class="knowledge long">
+			<li>JSON</li>
+			<li>Prototype</li>
+			<li>Mootools</li>
+			<li>Javascript</li>
+			<li>Google Analytics</li>
+		</ul>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
+	<h2>Epiphany MVC Framework</h2>
+	<div class="caption">
+		<p>
+			Envisioned and built by <a href="https://github.com/jmathai/" target="_blank">Jaisen Mathai</a>, I helped code a few sections and bug test and fix a few others. It is a fast, easy, clean and RESTful PHP framework. It does not do a lot of magic under the hood and is, by design, very simple and very powerful. It has an interesting RESTful API module where internal calls do not have to make an HTTP request, but still use the same API that external sources call.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge">
+			<li>PHP</li>
+			<li>MySQL</li>
+		</ul>
+		<div class="clearfix"></div>
+		<div class="actions">
+			<a href="https://github.com/jmathai/epiphany" target="_blank" class="btn btn-small btn-primary">View code on Github</a>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
+	<h2>TellUs</h2>
+	<div class="caption">
+		<p>
+			A design and marketing agency where I was the lead developer. I managed existing client sites, built new clients, and led our QA and ecommerce development. I also helped out on strategy, SEO, and analytics.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge short">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>Git</li>
+		</ul>
+		<ul class="knowledge medium">
+			<li>QA scripting</li>
+			<li>UI / UX</li>
+			<li>jQuery</li>
+			<li>Javascript</li>
+			<li>Codeigniter</li>
+		</ul>
+		<ul class="knowledge long">
+			<li>Google Analytics</li>
+			<li>Photoshop</li>
+		</ul>
+	</div>
+	<div class="clearfix"></div>
+</div>
+
+<div class="experience">
+	<h2>Strata-G Communications</h2>
+	<div class="caption">
+		<p>
+			A design and marketing agency where I was a web developer. I managed existing client sites, built new clients, and helped with analytics and SEO.<br />
+			<br />
+			Technologies used:<br />
+		</p>
+		<ul class="knowledge short">
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>MySQL</li>
+			<li>C#</li>
+		</ul>
+		<ul class="knowledge">
+			<li>ASP .NET</li>
+			<li>SQL Server</li>
+			<li>jQuery</li>
+			<li>Javascript</li>
+			<li>Google Analytics</li>
+			<li>Photoshop</li>
 		</ul>
 	</div>
 	<div class="clearfix"></div>
