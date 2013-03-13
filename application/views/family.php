75bf9c56e4ee33b00ce8db10704305bb1bae85ea
Kevin Hornschemeier <khornschemeier@gmail.com>
2013-03-13 01:05:25 -0400

    Started family page with hero image and basic layout Added versions to global.less in the head

---
+++ application/views/family.php
@@ -0,0 +1,14 @@
+<div class="row-fluid">
+	<div class="span5">
+		<section class="us">
+			<h2>Our Family</h2>
+			<p></p>
+		</section>
+	</div>
+	<div class="span6 offset1">
+		<aside class="photos">
+			<img src="/img/family/kevin-jennifer.jpg" alt="Kevin Hornschemeier, Jennifer Hornschemeier">
+			<img src="/img/family/everett.jpg" alt="Everett Hornschemeier">
+		</aside>
+	</div>
+</div>
